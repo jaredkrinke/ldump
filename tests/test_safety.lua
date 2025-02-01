240d154fcf46b0148a94697dc13c3752a24e9f41
girvel <widauka@ya.ru>
2025-02-01 16:08:29 +0500

    tests: Experiment with different safety measures on 5.1 and luajit

--- tests/test_safety.lua
+++ tests/test_safety.lua
@@ -1,3 +1,5 @@
+-- if _VERSION == "Lua 5.1" then return end  -- Lua 5.1 and LuaJIT are not safe
+
 local ldump = require("init")
 
 it("On-load safety", function()
@@ -5,7 +7,22 @@ it("On-load safety", function()
     print(123)
   end)
 
-  local ok, res = pcall(assert(load(malicious_data, nil, nil, ldump.get_safe_env())))
+  local ok, res
+  if _VERSION == "Lua 5.1" then
+    local f = function()
+      ok, res = pcall(assert(load(malicious_data)))
+    end
+
+    local env = ldump.get_safe_env()
+    env.pcall = pcall
+    env.assert = assert
+    setfenv(f, env)
+
+    f()
+  else
+    ok, res = pcall(assert(load(malicious_data, nil, nil, ldump.get_safe_env())))
+  end
+
   print(res)
   assert.is_false(ok)
 end)
