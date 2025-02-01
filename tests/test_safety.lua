a4a2dfb3b75b26ff8cb31c88d3cb143ca6d4474a
girvel <widauka@ya.ru>
2025-02-01 16:13:04 +0500

    tests: Continue safety experiments

--- tests/test_safety.lua
+++ tests/test_safety.lua
@@ -8,7 +8,7 @@ it("On-load safety", function()
   end)
 
   local ok, res
-  if _VERSION == "Lua 5.1" then
+  if _VERSION == "Lua 5.1" and type(jit) ~= "table" then
     local f = function()
       ok, res = pcall(assert(load(malicious_data)))
     end
@@ -35,7 +35,22 @@ it("Data safety", function()
   }))
 
   local deserialized = load(malicious_data, nil, nil, ldump.get_safe_env())()
-  local ok, res = pcall(function() return deserialized.innocent_looking_field end)
+
+  local ok, res
+  if _VERSION == "Lua 5.1" then
+    local f = function()
+      ok, res = pcall(function() return deserialized.innocent_looking_field end)
+    end
+
+    local env = ldump.get_safe_env()
+    env.pcall = pcall
+    setfenv(f, env)
+
+    f()
+  else
+    ok, res = pcall(function() return deserialized.innocent_looking_field end)
+  end
+
   print(res)
   assert.is_false(ok)
 end)
